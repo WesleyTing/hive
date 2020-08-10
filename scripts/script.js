d756546d927243724f90c5280a37484aadd58230
Wesley Ting <wesleyting3@gmail.com>
2020-08-09 17:36:56 -0700

    Added nav bar scroll effect

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,13 @@ const btn = document.querySelector('.nav-button');
 btn.addEventListener('click', function(){
     body.classList.toggle('show');
 });
+
+
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 500) {
+    $('.top-bar').css('background','black');
+    } else {
+    $('.top-bar').css('background','transparent');
+    }
+    });
