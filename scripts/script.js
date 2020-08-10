8e2a8343865c87aec5ba0a6e104313695d47d7cb
Wesley Ting <wesleyting3@gmail.com>
2020-08-10 09:34:41 -0700

    Added dropdown Nav bar
    Javascript dropdown nav bar
    functional desktop Nav Bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,54 @@
-// Nav Bar
+// Dropdown Menu
 const body = document.body;
-const btn = document.querySelector('.nav-button');
+const btnMenu = document.getElementById('btn-menu');
+const nav = document.getElementById('main-navigation');
 
-btn.addEventListener('click', function(){
-    body.classList.toggle('show');
+
+btnMenu.addEventListener('click', openMenu);
+btnMenu.addEventListener('mousedown', function(e){
+    e.preventDefault();
 });
 
+function openMenu(){
+    body.classList.toggle('show');
+	nav.classList.add('activated');
+}
 
+const mql = window.matchMedia('(min-width: 700px)');
+
+mql.addListener(removeTransition);
+
+function removeTransition(e){
+	if(e.matches){
+		body.classList.remove('show');
+		nav.classList.remove('activated');
+	}
+}
+
+
+// Change Nav Bar Color On Scroll
+$(window).scroll(function () {
+	if ($(window).scrollTop() >= 500) {
+	$('header').css('background','black');
+	} else {
+	$('header').css('background','transparent');
+	}
+	});
 
 $(window).scroll(function () {
-    if ($(window).scrollTop() >= 500) {
-    $('.top-bar').css('background','black');
-    } else {
-    $('.top-bar').css('background','transparent');
-    }
-    });
+	if ($(window).scrollTop() >= 500) {
+	$('nav.ul').css('background','black');
+	} else {
+	$('nav.ul').css('background','transparent');
+	}
+	});
+
+
+
+
+
+
+
+
+
+    
